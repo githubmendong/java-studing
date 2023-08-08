e38594105677f820efc3ded05a0a45977610c548
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-08 16:59:49 +0900

    prob05

--- practices02/src/main/java/prob05/Prob05.java
+++ practices02/src/main/java/prob05/Prob05.java
@@ -15,8 +15,27 @@ public class Prob05 {
 			// 정답 램덤하게 만들기
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
-
+//			System.out.println(correctNumber);
+			
+			System.out.println("수를 결정하였습니다.맞추어보세요.");
+			int guessCount = 0;
+			
+			while(true) {
+				System.out.print("숫자 입력: ");
+				int userGuess = scanner.nextInt();
+				guessCount++;
+				
+				if(userGuess < correctNumber) {
+					System.out.println("더 높");
+				}
+				else if(userGuess > correctNumber) {
+					System.out.println("더 낮");
+				}
+				else {
+					System.out.println("맞았습니다.");
+					break;
+				}
+			}
 			
 			//새 게임 여부 확인하기
 			System.out.print( "다시 하겠습니까(y/n)>>" );
