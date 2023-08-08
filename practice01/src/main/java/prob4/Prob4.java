fdd48389eecfbb4bc413a82a7be3b642319c519c
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-08 19:27:46 +0900

    Prob4

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -1,3 +1,14 @@
+// 문자열을 입력 받아서 아래와 같은 실행결과가 나타나도록
+//Prob4 클래스의 main 메소드를 완성하세요.
+// 입력 받은 문자열을 한 글짜씩 증가시키며 출력하는 프로그램입니다.
+// 문자열을 입력하세요. : Bat Man
+// B
+// Ba
+// Bat
+// Bat M
+// Bat Ma
+// Bat Man
+
 package prob4;
 
 import java.util.Scanner;
@@ -10,7 +21,10 @@ public class Prob4 {
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
 
-		
+		for (int i = 1; i <= text.length(); i++) {
+			System.out.println(text.substring(0, i));
+		}
+
 		scanner.close();
 	}
 
