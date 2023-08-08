d994ffa2642d95091a653989c9fd21c4b4260453
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-08 17:20:48 +0900

    prob02

--- practices02/src/main/java/prob02/Prob02.java
+++ practices02/src/main/java/prob02/Prob02.java
@@ -1,3 +1,5 @@
+//키보드에서 배열 크기만큼 정수를 입력받고 평균을 구하는 프로그램을 작성하시오
+
 package prob02;
 
 import java.util.Scanner;
@@ -11,10 +13,16 @@ public class Prob02 {
 		double sum = 0;
 
 		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		for (int i = 0; i<intArray.length; i++) {
+			System.out.println("정수 입력  : ");
+			intArray[i] = scanner.nextInt();
+			sum += intArray[i];
+		}
 		
-		/* 배열에 저장된 정수 값 더하기 */
 		
 		/* 출력 */
+		double ave = sum / intArray.length;
+		System.out.println("평균 : " + ave);
 		
 		/* 자원정리 */
 		scanner.close();
