131ccdd8ff50b6abc04abebb190667d32c804e07
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-09 17:06:23 +0900

    prob 04

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,15 @@
 package prob04;
 
 public class StringUtil {
+
+	public static String concatenate(String[] strArr) {
+		// TODO Auto-generated method stub
+		StringBuilder result = new StringBuilder();
+		
+		//배열의 모든 문자열을 순회
+		for (String str : strArr) {
+			result.append(str);
+		}
+		return result.toString();
+	}
 }
