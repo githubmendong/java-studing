efc23fcc52f3d26644aa7f7041953fc911eda61b
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-09 10:55:55 +0900

    10:50

---
+++ chapter03/src/main/java/chapter03/Good.java
@@ -0,0 +1,8 @@
+package chapter03;
+
+public class Good {
+	String name;
+	int price;
+	int countStock;
+	int coundSold;
+}
---
+++ chapter03/src/main/java/chapter03/GoodApp.java
@@ -0,0 +1,8 @@
+package chapter03;
+
+public class GoodApp {
+	public static void main(String[] args) {
+		Good camera = new Good();
+		
+	}
+}
