84de842a15f40d21520f88918a942e00716366bc
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-09 17:22:28 +0900

    git

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -4,7 +4,6 @@ public class Account {
 
 	public Account(String string) {
 		// TODO Auto-generated constructor stub
-		this.
 	}
 
 	public String getAccountNo() {
@@ -18,6 +17,14 @@ public class Account {
 	}
 
 	public void save(int i) {
+
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	public void deposit(int i) {
 		// TODO Auto-generated method stub
 		
 	}
