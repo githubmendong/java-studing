665b78cfa769962d9e874dbbecd10a2cf0efa38c
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-09 17:01:56 +0900

    prob 03

--- practice03/src/main/java/prob03/CurrencyConverter.java
+++ practice03/src/main/java/prob03/CurrencyConverter.java
@@ -2,7 +2,6 @@ package prob03;
 
 public class CurrencyConverter {
 	private static double rate;
-	
 
 	public static void setRate(double r) {
 		rate = r;
@@ -10,7 +9,7 @@ public class CurrencyConverter {
 
 	public static double toDollar(double won) {
 		// TODO Auto-generated method stub
-		return won/rate;
+		return won / rate;
 	}
 
 	public static double toKRW(double dollar) {
