a4257266d659a1063135a6d9ccd6e378f94245ba
shin-dongheon <120146699+githubmendong@users.noreply.github.com>
2023-08-11 01:38:29 +0900

    TV

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -7,27 +7,58 @@ public class TV {
 
 	public TV(int i, int j, boolean b) {
 		// TODO Auto-generated constructor stub
+		this.channel = 7;
+		this.volume = 20;
+		this.power = false;
 	}
 
+	// Getter 메소드
+	public int getChannel() {
+		return channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public boolean isPower() {
+		return power;
+	}
+
+	// 메소드 구현
 	public void power(boolean on) {
-		power = on;
-	}
-
-	public void channel(boolean up) {
+		this.power = on;
 	}
 
 	public void channel(int channel) {
-		this.channel = channel;
+		if (channel >= 1 && channel <= 255) {
+			this.channel = channel;
+		}
 	}
 
-	public void volume(boolean up) {
+	public void channel(boolean up) {
+		if (up && channel < 255) {
+			channel++;
+		} else if (!up && channel > 1) {
+			channel--;
+		}
 	}
 
 	public void volume(int volume) {
-		this.volume = volume;
+		if (volume >= 0 && volume <= 100) {
+			this.volume = volume;
+		}
+	}
+
+	public void volume(boolean up) {
+		if (up && volume < 100) {
+			volume++;
+		} else if (!up && volume > 0) {
+			volume--;
+		}
 	}
 
 	public void status() {
-		System.out.println("TV[power=" + (power ? "on" : "off") + ", channel=" + channel + ", volume=" + volume + "]");
+		System.out.println("TV [channel=" + channel + ", volume=" + volume + ", power=" + power + "]");
 	}
 }
--- chapter03/src/main/java/tv/WatchTV.java
+++ chapter03/src/main/java/tv/WatchTV.java
@@ -1,3 +1,29 @@
+//아래 WatchTV 클래스의 main 메쏘드를 실행할 수 있도록,  UML 그림과  요구 조건을  참조하여 TV 클래스를 정의 하세요.
+//1) 모든 필드는 private으로 접근 제어를 하고 getter만 작성합니다. (channel, volume, power 필드는  read-only)
+//2) channel, volume, power의 초기값을 각각 7, 20, false 로 초기화 하는 생성자 작성
+//3) 기본 생성자 오버로딩
+//4) void power( boolean on ) 메소드 구현
+//5) void channel( int channel ) 메소드 구현 (1~255 유지) 
+//6) void channel( boolean up ) 메소드 오버로딩 (1~255 유지, 1씩 증감)
+//7) void volume( int voluem ) 메소드 구현  ( 0 ~ 100 유지 )
+//8) void volume( boolean up ) 메소드 오버로딩 (0 ~ 100 유지, 1씩 증감)
+//9) void status() 메소드 구현( TV 정보 출력) 
+
+
+//TV
+//int channel
+//int volume
+//boolean power
+//getChannel()
+//getVolueme()
+//isPower()
+//power( boolean )
+//channel( int )
+//channel( boolean )
+//volume( int )
+//volume( boolean )
+//status();
+	
 package tv;
 
 public class WatchTV {
