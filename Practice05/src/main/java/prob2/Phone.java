52eeef9a3ff304a0d652c0f830dfb66c49a4b1e7
shin-dongheon <tlsejdgjs@naver.com>
2023-08-11 17:24:11 +0900

    5일차 prob2

--- Practice05/src/main/java/prob2/Phone.java
+++ Practice05/src/main/java/prob2/Phone.java
@@ -1,4 +1,12 @@
-package prob2;
+// PhoneApp가 제대로 작동되도록 MusicPhone을 상속한 SmartPhone Class를 작성하세요.
+// 실행결과
+//
+// 다운로드해서 음악재생
+// 통화기능시작
+// 앱실행
+
+
+        package prob2;
 
 public class Phone {
 	public void execute( String function ) {
@@ -9,3 +17,4 @@ public class Phone {
 		System.out.println( "통화기능시작" );
 	}
 }
+
