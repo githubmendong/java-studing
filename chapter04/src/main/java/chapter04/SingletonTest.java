761c84c2bd4d4781c182663ff5c6d7b993437f70
shin-dongheon <tlsejdgjs@naver.com>
2023-08-11 16:37:15 +0900

    5일차 Practice05

--- chapter04/src/main/java/chapter04/SingletonTest.java
+++ chapter04/src/main/java/chapter04/SingletonTest.java
@@ -2,9 +2,9 @@ package chapter04;
 
 public class SingletonTest {
     public static void main(String[] args) {
-        Singleton singleton1 = Singleton.getInstance();
-        Singleton singleton2 = Singleton.getInstance();
+//        Singleton singleton1 = Singleton.getInstance();
+//        Singleton singleton2 = Singleton.getInstance();
 
-        System.out.println(singleton1 == singleton2);
+//        System.out.println(singleton1 == singleton2);
     }
 }
