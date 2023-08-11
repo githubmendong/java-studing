b38740deeb5634e2a2831ff3236e77b9e258eb32
shin-dongheon <tlsejdgjs@naver.com>
2023-08-11 19:44:25 +0900

    5일차 prob4

--- Practice05/src/main/java/prob5/MainApp.java
+++ Practice05/src/main/java/prob5/MainApp.java
@@ -1,33 +1,49 @@
-//package prob5;
-//
-//public class MainApp {
-//
-//	public static void main(String[] args) {
-//		try {
-//			MyStack stack = new MyStack(3);
-//			stack.push("Hello");
-//			stack.push("World");
-//			stack.push("!!!");
-//			stack.push("java");
-//			stack.push(".");
-//
-//			while (stack.isEmpty() == false) {
-//				String s = stack.pop();
-//				System.out.println( s );
-//			}
-//
-//			System.out.println("======================================");
-//
-//			stack = new MyStack(3);
-//			stack.push("Hello");
-//
-//			System.out.println(stack.pop());
-//			System.out.println(stack.pop());
-//
-//		} catch ( MyStackException ex) {
-//			System.out.println( ex );
-//		}
-//
-//	}
-//
-//}
+
+//  MainApp.java와 Stack.java는 다음과 같이 주어집니다.
+//  Main 클래스를 실행하였을 때 아래와 같은 결과가 출력되도록
+//  MyStack 및 MyStackExcepetion 을 작성하세요.
+
+//[출력]
+//.
+//Java
+//!!!
+//World
+//Hello
+//==================================
+//Hello
+//Prob5.MyStackException: stack is empty
+
+
+package prob5;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		try {
+			MyStack stack = new MyStack(3);
+			stack.push("Hello");
+			stack.push("World");
+			stack.push("!!!");
+			stack.push("java");
+			stack.push(".");
+
+			while (stack.isEmpty() == false) {
+				String s = stack.pop();
+				System.out.println( s );
+			}
+
+			System.out.println("======================================");
+
+			stack = new MyStack(3);
+			stack.push("Hello");
+
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+
+		} catch ( MyStackException ex) {
+			System.out.println( ex );
+		}
+
+	}
+
+}
--- Practice05/src/main/java/prob5/MyStack.java
+++ Practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,14 @@
-//package prob5;
-//
-//public class MyStack {
-//}
+package prob5;
+
+public class MyStack {
+    private static final long serialVersionUID = 1L;
+
+    public MyStack() {
+        super();
+    }
+
+    public MyStack(String message) {
+        super(message);
+    }
+
+}
