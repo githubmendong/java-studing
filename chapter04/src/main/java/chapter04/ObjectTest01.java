d7c7aecb388e9ab2e810be6040c4871de729fff8
shin-dongheon <tlsejdgjs@naver.com>
2023-08-11 10:55:49 +0900

    4일차

--- chapter04/src/main/java/chapter04/ObjectTest01.java
+++ chapter04/src/main/java/chapter04/ObjectTest01.java
@@ -4,10 +4,15 @@ public class ObjectTest01 {
 
 	public static void main(String[] args) {
 		Point point = new Point();
-		
-		Class klass = point.getClass();
-		System.out.println(klass);
-		//test
+
+//		Class klass = point.getClass();
+		System.out.println(point.getClass());
+		System.out.println(point.hashCode()); 	//address X
+												// reference x
+												// address 기반의 해싱값 o
+
+		System.out.println(point.toString()); 	//getClass() + "@" + hashCode()
+		System.out.println(point);
 	}
 
 }
