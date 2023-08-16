96be5b2ccca77afd5c060fc06c780331a5b6c126
shin-dongheon <tlsejdgjs@naver.com>
2023-08-16 15:13:17 +0900

    6일차 io - 1

--- chapter04/src/main/java/collection/Gugudan.java
+++ chapter04/src/main/java/collection/Gugudan.java
@@ -1,11 +1,11 @@
-package collection;
-
-public class Gugudan {
-    private int lValue;
-    private int rValue;
-
-
-    public Gugudan(int lValue, int rValue){
-        return "Gugudna [lValue =" +lValue + rValue;
-    }
-}
+//package collection;
+//
+//public class Gugudan {
+//    private int lValue;
+//    private int rValue;
+//
+//
+//    public Gugudan(int lValue, int rValue){
+//        return "Gugudna [lValue =" +lValue + rValue;
+//    }
+//}
